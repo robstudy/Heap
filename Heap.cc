ee3a7007dd90d5b51416f0021ef9f32c8c1dc8a3
Robert Garza <robstudent1988@gmail.com>
2018-04-12 11:16:08 -0700

    Implemented Max_Heapify and BuildHeap functions. Test in main

---
+++ Heap.cc
@@ -0,0 +1,38 @@
+#include <cmath>
+template <typename T>
+void swap(T* a, T* b)
+{
+	T temp = a;
+	b = a;
+	a = temp;
+};
+
+template <typename T>
+void Max_Heapify(T* A, int index, int size) {
+	int left = 2 * index;
+	int right = 2 * index + 1;
+	int maxIndex = index;
+	
+	if ((left <= size) && (A[left] > A[index]))
+		maxIndex = left;
+	if((right <= size) && (A[right] > A[maxIndex]))
+		maxIndex = right;
+	if(maxIndex != index) {
+		T tmp = A[index];
+		A[index] = A[maxIndex];
+		A[maxIndex] = tmp;
+		Max_Heapify(A, maxIndex, size);
+	}
+};
+
+template <typename T>
+void BuildHeap(T* A, int size){
+	size--;
+	for (int i = size/2; i >= 0; i--)
+			Max_Heapify(A, i, size);
+};
+
+template <typename T>
+void HeapSort(T* A, int size) {
+	//Implement later
+};
--- main.cc
+++ main.cc
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Heap.cc"
 
 int main() {
+	int size = 7;
+	int *A;
+	A = new int[size];
+	for(int i = 0; i < size; i++) A[i] = i;
 	std::cout << "Heap!" << std::endl;
+	for(int i=0; i < size; i++) std::cout << A[i] << " ";
+	std::cout << std::endl;
+	BuildHeap(A,size);
+	for(int i=0; i < size; i++) std::cout << A[i] << " ";
+	std::cout << std::endl;
 	return 0;
 }
